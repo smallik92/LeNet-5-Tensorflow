8a58ee8378cc8ec7f463f58cc2a5efb4cd46a3d7
Sruti Mallik <49848345+smallik92@users.noreply.github.com>
2020-07-07 17:12:54 -0500

    Add files via upload

---
+++ LeNet5_oop.py
@@ -0,0 +1,98 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, Flatten, Conv2D, AveragePooling2D, MaxPooling2D
+from keras.models import Model, load_model
+from keras.initializers import glorot_uniform
+
+class LeNet5():
+
+    def __init__(self, X_train, y_train, X_test, y_test, num_classes, hparameters):
+        self.X_train = X_train
+        self.y_train = y_train
+        assert(len(self.X_train) == len(self.y_train))
+
+        self.X_test = X_test
+        self.y_test = y_test
+        assert(len(self.X_test) == len(self.y_test))
+
+        self.num_classes = num_classes
+        self.hparameters = hparameters
+
+    def LeNet5_architecture(self, hparameters, num_classes, input_shape):
+
+        """
+        implements the architecture of LeNet5.
+        Conv2D -> Avg pool -> Conv2D -> Avg pool -> Conv2D -> Flatten -> Fully Connected -> Output
+        """
+
+        # Convolution layer (C1) hyperparameters
+        s1 = hparameters["s1"]
+        f1 = hparameters["f1"]
+        n1 = hparameters["n1"]
+
+        # Average pooling layer(S2) hyperparameters
+        s2 = hparameters["s2"]
+        f2 = hparameters["f2"]
+
+        # Convolutional layer (C3) hyperparameters
+        s3 = hparameters["s3"]
+        f3 = hparameters["f3"]
+        n3 = hparameters["n3"]
+
+        # Average pooling layers (S4) hyperparameters
+        s4 = hparameters["s4"]
+        f4 = hparameters["f4"]
+
+        # Convolutional layer (C5) hyperparameters
+        s5 = hparameters["s5"]
+        f5 = hparameters["f5"]
+        n5 = hparameters["n5"]
+
+        X_input = Input(input_shape)
+        X = X_input
+
+        # Convolution layer 1
+        X = Conv2D(n1, (f1,f1), strides = (s1, s1), padding = 'valid', name = 'C1', kernel_initializer = glorot_uniform(seed = 0))(X)
+        # Average pooling
+        X = AveragePooling2D(pool_size= (f2,f2), strides = (s2,s2), padding = 'valid', name = 'S2')(X)
+        # Activation
+        X = Activation('tanh')(X)
+        # Convolution layer 2
+        X = Conv2D(n3, (f3,f3), strides = (s3, s3), padding = 'valid', name = 'C3', kernel_initializer = glorot_uniform(seed = 0))(X)
+        #Average pooling
+        X = AveragePooling2D(pool_size= (f4,f4), strides = (s4,s4), padding = 'valid', name = 'S4')(X)
+        # Activation
+        X = Activation('tanh')(X)
+        # Convolutional layer 3
+        X = Conv2D(n5, (f5,f5), strides = (s5, s5), padding = 'valid', name = 'C5', kernel_initializer = glorot_uniform(seed = 0))(X)
+        # Activation
+        X = Activation('tanh')(X)
+        # Flatten
+        X = Flatten()(X)
+        # Fully Connected layer
+        X = Dense(num_classes, activation = 'softmax', name = 'FC', kernel_initializer = glorot_uniform(seed = 0))(X)
+
+        #create model
+        model = Model(inputs = X_input, outputs = X, name = 'LeNet5')
+
+        return model
+
+    def train(self, model, num_epochs = 70, batch_size = 32, learning_rate = 0.001):
+
+        assert(num_epochs>0 and batch_size>0 and learning_rate>0)
+
+        opt = keras.optimizers.Adam(learning_rate = learning_rate)
+
+        #train
+        print('Training model....')
+        model.compile(optimizer = opt, loss = 'categorical_cross_entropy', metrics = ['accuracy'])
+        history = model.fit(self.X_train, self.y_train, epochs = num_epochs, batch_size = batch_size)
+
+        return model, history
+
+    def evaluate(self, model):
+        preds = model.evaluate(self.X_test, self.y_test)
+
+        return preds[1]
---
+++ lenet_main.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from LeNet5_oop import LeNet5
+import tensorflow_datasets as tfds
+
+
+def load_dataset():
+    """
+    Loads MNIST dataset as numpy array
+    """
+    X_train, y_train = tfds.as_numpy(tfds.load('mnist', split = 'train', shuffle_files = True , batch_size = -1, as_supervised = True))
+    X_test, y_test = tfds.as_numpy(tfds.load('mnist', split = 'test', shuffle_files = True , batch_size = -1, as_supervised = True))
+
+    #zero padding for LeNet 5's input size
+    X_train = np.pad(X_train, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+    X_test = np.pad(X_test, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+
+    classes = np.unique(y_train)
+
+    return X_train, y_train, X_test, y_test, classes
+
+def convert_to_one_hot(Y, C):
+
+    """
+    Converts labels to one-hot vector
+    """
+
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
+
+
+def main():
+
+    X_train, y_train, X_test, y_test, classes = load_dataset()
+    num_classes = len(classes)
+    y_train = convert_to_one_hot(y_train, num_classes).T
+    y_test = convert_to_one_hot(y_test, num_classes).T
+
+    (m, n_H0, n_W0, n_C0) = X_train.shape
+
+    hparameters = {"s1": 1, "f1": 5, "n1": 6,
+                    "s2":2, "f2":2,
+                    "s3":1, "f3": 5, "n3": 16,
+                    "s4":2, "f4": 2,
+                    "s5":1, "f5": 5, "n5": 120}
+
+    lenet5_network = LeNet5(X_train, y_train, X_test, y_test, num_classes, hparameters)
+    model = lenet5_network.LeNet5_architecture(hparameters, num_classes, input_shape = (n_H0, n_W0, n_C0) )
+    model, history = lenet5_network.train(model, num_epochs = 2, batch_size = 32, learning_rate = 0.001)
+    test_accuracy = lenet5_network.evaluate(model)
+
+    print('Accuracy on test set: {:.3f} %'.format(test_accuracy*100))
+
+    plt.plot(history.history['loss'])
+    plt.title('Training error')
+    plt.xlabel('epoch')
+    plt.ylabel('Loss')
+    plt.show()
+
+
+
+main()
